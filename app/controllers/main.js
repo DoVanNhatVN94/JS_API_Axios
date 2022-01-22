b9ece1989202bcace6b215a8daa02cfeeed6ca2a
DoVanNhatVN94 <dovannhatdn94@gmail.com>
2022-01-22 20:51:05 +0700

    up3

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -33,7 +33,7 @@ let hienThiThongTin = (array) => {
                 <td>
                     <button class="btn btn-info" data-toggle="modal"
                     data-target="#myModal" onclick="xemChiTiet(${info.id})">Xem</button>
-                    <button class="btn btn-danger" onclick="xoaSanPham(${info.id})">Xoa</button>
+                    <button class="btn btn-danger" onclick="xoaNguoiDung(${info.id})">Xoa</button>
                 </td>
             </tr>
         `;
@@ -66,17 +66,26 @@ let themNguoiDung = () => {
     let isValid = true;
 
     isValid &= validation.checkEmpty(taikhoan, "spanTaiKhoan", "Tài Khoản Ko Để Trống")
-        && validation.checkID(taikhoan,"spanTaiKhoan","Tai khoang Bi Trung",infoPeople.mang )
-        ;
-    isValid &= validation.checkEmpty(hoten, "spanHoTen", "Họ Tên Ko Để Trống");
-    isValid &= validation.checkEmpty(matkhau, "spanMatKhau", "Mật Khẩu Ko Để Trống");
-    isValid &= validation.checkEmpty(email, "spanEmail", "Email Ko Để Trống");
+        && validation.checkID(taikhoan, "spanTaiKhoan", "Tai khoang Bi Trung", infoPeople.mang);
+
+    isValid &= validation.checkEmpty(hoten, "spanHoTen", "Họ Tên Ko Để Trống")
+    && validation.checkName(hoten,"spanHoTen","Họ Tên Ko Nên chứa Số Và Ký Tự Đăc Biệt");
+
+    isValid &= validation.checkEmpty(matkhau, "spanMatKhau", "Mật Khẩu Ko Để Trống")
+    && validation.checkPass(matkhau, "spanMatKhau", "Mật KHẩu ko Đúng ĐỊnh Dạng");
+
+    isValid &= validation.checkEmpty(email, "spanEmail", "Email Ko Để Trống")
+    && validation.checkEmail(email, "spanEmail","Email ko Đúng ĐỊnh Dạng");
+
     isValid &= validation.checkEmpty(hinhanh, "spanHinhAnh", "Hình Ảnh Ko Để Trống");
+
     isValid &= validation.checkSelect("loaiNguoiDung", "spanloaiNguoiDung", "Chọn Loai ND");
+
     isValid &= validation.checkSelect("loaiNgonNgu", "spanloaiNgonNgu", "Chọn Loai Ngôn Ngữ");
 
 
-    isValid &= validation.checkEmpty(mota, "spanMoTa", "Mô Tả Ko Để Trống");
+    isValid &= validation.checkEmpty(mota, "spanMoTa", "Mô Tả Ko Để Trống")
+    && validation.check60(mota,"spanMoTa","Mô Tả ko Nên Vượt Quá 60 ký Tự");
 
     if (isValid) {
         let info = new NguoiDung(taikhoan, hoten, matkhau, email, loaiND, ngonngu, mota, hinhanh);
@@ -102,7 +111,7 @@ let themNguoiDung = () => {
 
 }
 
-function xoaSanPham(id) {
+function xoaNguoiDung(id) {
     infoPeople.xoaTV(id)
         .then(function (result) {
             //thành công
@@ -134,7 +143,7 @@ function xemChiTiet(id) {
             document.getElementById("MoTa").value = result.data.moTa;
 
             document.querySelector(".modal-footer").innerHTML = `<button class="btn btn-info" onclick="reset()">Reset</button>
-            <button class="btn btn-success" onclick="capNhapSanPham('${result.data.id}')">Cập Nhập</button>`;
+            <button class="btn btn-success" onclick="capNhapNguoiDung('${result.data.id}')">Cập Nhập</button>`;
             document.getElementById("TaiKhoan").disabled = true;
         })
         .catch(function (error) {
@@ -143,7 +152,7 @@ function xemChiTiet(id) {
         })
 }
 
-function capNhapSanPham(id) {
+function capNhapNguoiDung(id) {
 
     let taikhoan = document.getElementById("TaiKhoan").value;
     let hoten = document.getElementById("HoTen").value;
@@ -157,15 +166,25 @@ function capNhapSanPham(id) {
     let isValid = true;
 
     isValid &= validation.checkEmpty(taikhoan, "spanTaiKhoan", "Tài Khoản Ko Để Trống");
-    isValid &= validation.checkEmpty(hoten, "spanHoTen", "Họ Tên Ko Để Trống");
-    isValid &= validation.checkEmpty(matkhau, "spanMatKhau", "Mật Khẩu Ko Để Trống");
-    isValid &= validation.checkEmpty(email, "spanEmail", "Email Ko Để Trống");
+
+    isValid &= validation.checkEmpty(hoten, "spanHoTen", "Họ Tên Ko Để Trống")
+    && validation.checkName(hoten,"spanHoTen","Họ Tên Ko Nên chứa Số Và Ký Tự Đăc Biệt");
+
+    isValid &= validation.checkEmpty(matkhau, "spanMatKhau", "Mật Khẩu Ko Để Trống")
+    && validation.checkPass(matkhau, "spanMatKhau", "Mật KHẩu ko Đúng ĐỊnh Dạng");
+
+    isValid &= validation.checkEmpty(email, "spanEmail", "Email Ko Để Trống")
+    && validation.checkEmail(email, "spanEmail","Email ko Đúng ĐỊnh Dạng");
+
     isValid &= validation.checkEmpty(hinhanh, "spanHinhAnh", "Hình Ảnh Ko Để Trống");
 
+    isValid &= validation.checkSelect("loaiNguoiDung", "spanloaiNguoiDung", "Chọn Loai ND");
+
+    isValid &= validation.checkSelect("loaiNgonNgu", "spanloaiNgonNgu", "Chọn Loai Ngôn Ngữ");
 
 
-
-    isValid &= validation.checkEmpty(mota, "spanMoTa", "Mô Tả Ko Để Trống");
+    isValid &= validation.checkEmpty(mota, "spanMoTa", "Mô Tả Ko Để Trống")
+    && validation.check60(mota,"spanMoTa","Mô Tả ko Nên Vượt Quá 60 ký Tự");
 
     if (isValid) {
         let info = new NguoiDung(taikhoan, hoten, matkhau, email, loaiND, ngonngu, mota, hinhanh)
@@ -197,4 +216,7 @@ function resetSpan() {
 let reset = () => {
     resetSpan();
     document.getElementById("myModal").reset();
+    document.getElementById("TaiKhoan").disabled = false;
 }
+
+document.querySelector(".close").onclick = reset;
